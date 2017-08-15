e52d3f63647e175077e4d6f45c01a6a9cbd9ae23
Devraj Mehta <devm33@gmail.com>
2017-08-15 18:14:07 -0400

    basic clock structure complete

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+
+export default class App extends Component {
   render() {
+    const months = ['January', 'February', 'March', 'April', 'May', 'June',
+      'July', 'August', 'September', 'October', 'November', 'December'];
+    var monthsDivs = [];
+    for (let i = 0; i < months.length; i++) {
+      let m = months[i];
+      let classes = "month " + m;
+      monthsDivs.push(<div key={m} className={classes}>{m}</div>);
+    }
+    // Calculate arm position
+    let d = new Date();
+    let start = new Date(d.getFullYear(), 0, 0, 0, 0, 0);
+    let end =  new Date(d.getFullYear() + 1, 0, 0, 0, 0, 0);
+    let p = (d - start) / (end - start);
+    console.log(p);
+    let deg = 360 * p - 180; // substracting 180 since the arm rests at june
+    let armStyle = {
+      transform: 'rotate(' + deg + 'deg)'
+    };
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div className="clock">
+          {monthsDivs}
+          <div className="arm" style={armStyle}></div>
+      <div className="dot"></div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
 }
-
-export default App;
