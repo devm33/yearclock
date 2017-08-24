7d6363588b7e73103a0bce59170410dcec00eb56
Devraj Mehta <devm33@gmail.com>
2017-08-23 22:11:37 -0400

    update arm once an hour

--- src/App.js
+++ src/App.js
@@ -4,30 +4,51 @@ import './App.css';
 
 
 export default class App extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {rotation: 0};
     const months = ['January', 'February', 'March', 'April', 'May', 'June',
       'July', 'August', 'September', 'October', 'November', 'December'];
-    var monthsDivs = [];
+    this.monthsDivs = [];
     for (let i = 0; i < months.length; i++) {
       let m = months[i];
       let classes = "month " + m;
-      monthsDivs.push(<div key={m} className={classes}>{m}</div>);
+      this.monthsDivs.push(<div key={m} className={classes}>{m}</div>);
     }
-    // Calculate arm position
+  }
+
+  tick() {
     let d = new Date();
     let start = new Date(d.getFullYear(), 0, 0, 0, 0, 0);
     let end =  new Date(d.getFullYear() + 1, 0, 0, 0, 0, 0);
     let p = (d - start) / (end - start);
-    let deg = 360 * p - 180; // substracting 180 since the arm rests at june
-    let armStyle = {
-      transform: 'rotate(' + deg + 'deg)'
-    };
+    let deg = 360 * p - 180; // substracting 180 since the arm defaults to june
+    this.setState({
+      rotation: deg
+    });
+  }
+
+  componentDidMount() {
+    this.tick()
+    this.timerID = setInterval(
+      () => this.tick(),
+      60 * 60 * 1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  render() {
     return (
       <div className="App">
         <div className="clock">
-          {monthsDivs}
-          <div className="arm" style={armStyle}></div>
-      <div className="dot"></div>
+          {this.monthsDivs}
+          <div className="arm"
+               style={{transform: 'rotate(' + this.state.rotation + 'deg)'}}>
+          </div>
+          <div className="dot"></div>
         </div>
       </div>
     );
